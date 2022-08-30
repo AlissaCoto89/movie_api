d56008b7811bcfbd7a8e4843400842676c1d1ac5
AlissaCoto89 <acoto189@gmail.com>
2022-08-30 17:25:14 -0400

    added GET routes

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+
+app.get('/', (req, res) => {
+    res.send('Welcome to MyFlix!');
+});
+
