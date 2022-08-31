eff9183a435a0a049909e570f4dfe2fefa013541
AlissaCoto89 <acoto189@gmail.com>
2022-08-30 20:39:22 -0400

    express static

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
-  });
-
-app.get('/', (req, res) => {
-    res.send('Welcome to MyFlix!');
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
 });
 
+app.get("/", (req, res) => {
+  res.send("Welcome to MyFlix!");
+});
+
+app.get('/documentation.html', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.use("/documentation.html", express.static("public"));
+
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
