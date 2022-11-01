a9ace91bf900fcc617920bcb70291fdcbfa7a36d
AlissaCoto89 <acoto189@gmail.com>
2022-11-01 18:30:34 -0400

    undid auth

--- index.js
+++ index.js
@@ -19,22 +19,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require("cors");
-const allowedOrigins = [
-  "http://localhost:1234",
-  "https://my-flix-db-akc.herokuapp.com"]
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message =
-        "The CORS policy for this application doesn’t allow access from origin " +
-        origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  },
-})
-);
+app.use(cors());
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
@@ -81,8 +66,8 @@ app.get(
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Movies.findOne({ Title: req.params.Title })
-      .then((movie) => {
-        res.json(movie);
+      .then((movies) => {
+        res.json(movies);
       })
       .catch((error) => {
         console.error(error);
@@ -90,40 +75,6 @@ app.get(
       });
   }
 );
-
-app.post(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.findOne({ Title: req.body.title })
-      .then((movie) => {
-        if (movie) {
-          return res.status(400).send(req.body.title + " already exists");
-        } else {
-          Movies.create({
-            Title: req.body.Title,
-            Genre: req.body.Genre,
-            Director: req.body.Director,
-            Description: req.body.Description,
-            ImagePath: req.body.ImagePath || "#",
-            Featured: req.body.Featured ? req.body.Featured : false,
-          })
-            .then((movie) => {
-              res.status(201).json(movie);
-            })
-            .catch((error) => {
-              console.error(error);
-              res.status(500).send("Error: " + error);
-            });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("Error: " + error);
-      });
-  }
-);
-
 
 app.get(
   "/movies/genre/:Name",
@@ -173,7 +124,7 @@ app.post(
       return res.status(422).json({ errors: errors.array() });
     }
 
-    const hashedPassword = Users.hashPassword(req.body.Password);
+    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOne({ Username: req.body.Username })
       .then((user) => {
         if (user) {
@@ -278,26 +229,6 @@ app.put(
   }
 );
 
-app.post(
-  "/users/:Username/movies/:MovieID",
-  (req, res) => {
-    Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      {
-        $push: { FavoriteMovies: req.params.MovieID },
-      },
-      { new: true },
-      (err, updatedUser) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error: " + error);
-        } else {
-          res.json(updatedUser);
-        }
-      }
-    );
-  }
-);
 
 app.delete(
   "/users/:Username",
@@ -311,36 +242,12 @@ app.delete(
           res.status(200).send(req.params.Username + " was deleted.");
         }
       })
-      .catch((err) => {
+      .catch((error) => {
         console.error(error);
         res.status(500).send("Error: " + error);
       });
   }
 );
-
-app.delete(
-  "/users/:Username/movies/:MovieID",
-  (req, res) => {
-    Users.findOneAndUpdate({ Username: req.params.Username },
-      {
-        $pull:{ FavoriteMovies: req.params.MovieID },
-      },
-      { new: true },
-      (err, updatedUser) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error: " + err);
-        } else {
-          res.json(updatedUser);
-        }
-      }
-    );
-
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("Error: " + error);
-      });
 
 
 const port = process.env.PORT || 8080;
