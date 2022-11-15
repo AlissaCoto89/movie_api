7a0017f7f0a7d1ef87d0d354d43ced0500d2c420
AlissaCoto89 <acoto189@gmail.com>
2022-11-15 14:55:02 -0500

    fixed CORS

--- index.js
+++ index.js
@@ -19,7 +19,25 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require("cors");
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:1234",
+  "http:///localhost:8080",
+  "https://myflix-akc.netlify.app",
+  "https://my-flix-db-akc.herokuapp.com"
+];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message =
+        "The CORS policy for this application does not allow access from origin " +
+        origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  },
+})
+);
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
